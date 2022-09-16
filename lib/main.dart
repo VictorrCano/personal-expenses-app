68b20b266db3d9262d8a895666f6a9b7a110aa7b
Victor <victorcano.apple@gmail.com>
2022-09-16 16:19:17 +1000

    refactored main.dart into separate widgets, added dynamic transaction list + scrolling

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:uuid/uuid.dart';
 
-import './transcation.dart';
+import './models/transcation.dart';
+import './widgets/user_transactions.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,74 +13,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Expenses Tracking App',
+      theme: ThemeData(primarySwatch: Colors.purple),
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction('t1', 89.99, 'New shoes', DateTime.now()),
-    Transaction('t2', 39.99, 'Dinner', DateTime.now()),
-    Transaction('t3', 19.99, 'Shampoo', DateTime.now())
-  ];
-
   @override
   Widget build(BuildContext context) {
+    //print(titleInput);
+    //print(costInput);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Expenses Tracking'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Card(
-            margin: EdgeInsets.all(20),
-            elevation: 5,
-            color: Colors.purple[300],
-            child: Container(
-              width: double.infinity,
-              child: const Text(
-                'Chart to be added',
+      body: SingleChildScrollView(
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Card(
+              margin: EdgeInsets.all(20),
+              elevation: 5,
+              color: Colors.purple[300],
+              child: Container(
+                width: double.infinity,
+                child: const Text(
+                  'Chart to be added',
+                ),
               ),
             ),
-          ),
-          Column(
-            children: transactions
-                .map(
-                  (transaction) => Container(
-                    //height: 30,
-                    width: double.infinity,
-                    margin: EdgeInsets.all(10),
-                    child: Card(
-                      color: Colors.purple[100],
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Container(
-                            padding: EdgeInsets.all(20),
-                            child: Text(transaction.cost.toString() + ' \$'),
-                          ),
-                          Column(
-                            children: [
-                              Text(
-                                transaction.title + '\n',
-                              ),
-                              Text(
-                                transaction.date.toString(),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                )
-                .toList(),
-          ),
-        ],
+            UserTransactions(),
+          ],
+        ),
       ),
     );
   }
